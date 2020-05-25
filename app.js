38d268baa73c5737c32cd97a5425de13073222b9
lamphc <lamphc@live.com>
2020-05-25 23:17:09 +0800

    use bt

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const Router = require('koa-router')
 const { Wechat } = require('wechat-jssdk')
 // 创建wechat验证实例：传入微信公众号测试appId和appSecret
 const wx = new Wechat({
-  appId: 'wxe733093faa04b8aa',
-  appSecret: '24f11b6815515016170c4ffeb6977e65'
-  // appId: 'wx7cfd1bd1af42d511',
-  // appSecret: '2fae9e51b3c6e89678db2ae74cf64bc3'
+  // appId: 'wxe733093faa04b8aa',
+  // appSecret: '24f11b6815515016170c4ffeb6977e65'
+  appId: 'wx7cfd1bd1af42d511',
+  appSecret: '2fae9e51b3c6e89678db2ae74cf64bc3'
 });
 
 // 创建Koa实例
@@ -19,7 +19,7 @@ const router = new Router()
 
 // 定义接口
 router.get('/check', async (ctx) => {
-  let res = await wx.jssdk.getSignature('http://localhost:8000/')
+  let res = await wx.jssdk.getSignature('http://localhost:3000/')
   ctx.body = res
 })
 // 使用路由
@@ -37,9 +37,9 @@ app.use(views(__dirname + '/views', {
 
 app.use(async ctx => {
   // 获取验证信息
-  let res = await wx.jssdk.getSignature('http://localhost:8000/')
+  let res = await wx.jssdk.getSignature('http://localhost:3000/')
   // 同步渲染页面并返回
   await ctx.render('index.hbs', res)
 })
 
-app.listen(8000, () => console.log('run...'))
+app.listen(3000, () => console.log('run...'))
